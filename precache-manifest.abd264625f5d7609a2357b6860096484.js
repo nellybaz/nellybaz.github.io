66945a53ee9611fcf8564ff704b17943698ed17a
Nelson Bassey <nellybaz10@gmail.com>
2019-03-14 16:47:20 +0200

    profile page

---
+++ precache-manifest.abd264625f5d7609a2357b6860096484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6447595cc241d604e0f1a04cfb797e0c",
+    "url": "/static/media/NelsonBassey_Resume.6447595c.pdf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "964659f97025f2453cf9",
+    "url": "/static/js/main.964659f9.chunk.js"
+  },
+  {
+    "revision": "772dda025ac13a161146",
+    "url": "/static/js/2.772dda02.chunk.js"
+  },
+  {
+    "revision": "964659f97025f2453cf9",
+    "url": "/static/css/main.2109cf59.chunk.css"
+  },
+  {
+    "revision": "aefb9323da3b0556f0c2fd8147580e2b",
+    "url": "/index.html"
+  }
+];
